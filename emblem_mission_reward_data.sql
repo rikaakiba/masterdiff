1a1bfebb354b95df82f6dfaf9d0763eeb07dd830
Akiba Rika <perfectt93@gmail.com>
2021-05-07 11:52:49 +0900

    10028400

--- emblem_mission_reward_data.sql
+++ emblem_mission_reward_data.sql
@@ -718,3 +718,5 @@ INSERT INTO "emblem_mission_reward_data" VALUES (/*id*/832, /*mission_reward_id*
 INSERT INTO "emblem_mission_reward_data" VALUES (/*id*/833, /*mission_reward_id*/56011476, /*reward_type*/15, /*reward_id*/10201476, /*reward_num*/1, /*icon_type*/1002);
 INSERT INTO "emblem_mission_reward_data" VALUES (/*id*/834, /*mission_reward_id*/56011477, /*reward_type*/15, /*reward_id*/10201477, /*reward_num*/1, /*icon_type*/1003);
 INSERT INTO "emblem_mission_reward_data" VALUES (/*id*/835, /*mission_reward_id*/56011477, /*reward_type*/8, /*reward_id*/91002, /*reward_num*/50, /*icon_type*/0);
+INSERT INTO "emblem_mission_reward_data" VALUES (/*id*/836, /*mission_reward_id*/56011484, /*reward_type*/15, /*reward_id*/10201484, /*reward_num*/1, /*icon_type*/1003);
+INSERT INTO "emblem_mission_reward_data" VALUES (/*id*/837, /*mission_reward_id*/56011484, /*reward_type*/8, /*reward_id*/91002, /*reward_num*/100, /*icon_type*/0);
