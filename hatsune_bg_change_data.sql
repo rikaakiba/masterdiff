5701e5f05f0cb4da465dd37466b1c02ff90bd722
Akiba Rika <perfectt93@gmail.com>
2022-09-30 11:51:14 +0900

    10041800

--- hatsune_bg_change_data.sql
+++ hatsune_bg_change_data.sql
@@ -66,3 +66,4 @@ INSERT INTO "hatsune_bg_change_data" VALUES (/*id*/79, /*area_id*/10099, /*condi
 INSERT INTO "hatsune_bg_change_data" VALUES (/*id*/80, /*area_id*/10100, /*condition_type*/3, /*condition_id*/5100003, /*target_type*/2, /*bg_after_change_id*/500837);
 INSERT INTO "hatsune_bg_change_data" VALUES (/*id*/82, /*area_id*/10101, /*condition_type*/3, /*condition_id*/5074001, /*target_type*/2, /*bg_after_change_id*/501383);
 INSERT INTO "hatsune_bg_change_data" VALUES (/*id*/83, /*area_id*/10101, /*condition_type*/3, /*condition_id*/5074001, /*target_type*/2, /*bg_after_change_id*/501383);
+INSERT INTO "hatsune_bg_change_data" VALUES (/*id*/84, /*area_id*/10102, /*condition_type*/3, /*condition_id*/5102002, /*target_type*/2, /*bg_after_change_id*/501873);
