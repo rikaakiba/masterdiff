5e3e4ec3f5c83b493652add377954f5dde02a5f1
Akiba Rika <perfectt93@gmail.com>
2022-10-05 11:51:05 +0900

    10041900

---
+++ ex_equipment_recycle_reward.sql
@@ -0,0 +1,2 @@
+CREATE TABLE 'ex_equipment_recycle_reward' ('rarity' INTEGER NOT NULL, 'enhance_pt_coefficient' INTEGER NOT NULL, 'coin_coefficient' INTEGER NOT NULL, PRIMARY KEY('rarity'));
+
---
+++ travel_quest_result.sql
@@ -0,0 +1,2 @@
+CREATE TABLE 'travel_quest_result' ('situation_id' INTEGER NOT NULL, 'drama_id' INTEGER NOT NULL, PRIMARY KEY('situation_id'));
+
