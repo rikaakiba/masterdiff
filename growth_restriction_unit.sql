e5c1e84c4a045b686f93a4408054dd54d128e654
Akiba Rika <perfectt93@gmail.com>
2022-11-30 11:51:14 +0900

    10043400

--- growth_restriction_unit.sql
+++ growth_restriction_unit.sql
@@ -13,3 +13,4 @@ INSERT INTO "growth_restriction_unit" VALUES (/*id*/11, /*growth_id*/13, /*unit_
 INSERT INTO "growth_restriction_unit" VALUES (/*id*/12, /*growth_id*/14, /*unit_id*/122401);
 INSERT INTO "growth_restriction_unit" VALUES (/*id*/13, /*growth_id*/15, /*unit_id*/122801);
 INSERT INTO "growth_restriction_unit" VALUES (/*id*/14, /*growth_id*/16, /*unit_id*/123501);
+INSERT INTO "growth_restriction_unit" VALUES (/*id*/15, /*growth_id*/17, /*unit_id*/124101);
