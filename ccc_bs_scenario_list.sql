ebc38c3104e517b8a59f282d469fc9a845ae60ea
Akiba Rika <perfectt93@gmail.com>
2023-11-30 12:19:30 +0900

    10052100

---
+++ ccc_bs_scenario_list.sql
@@ -0,0 +1,2 @@
+CREATE TABLE 'ccc_bs_scenario_list' ('ccc_scenario_id' INTEGER NOT NULL, PRIMARY KEY('ccc_scenario_id'));
+
---
+++ nyd_setting.sql
@@ -0,0 +1,2 @@
+CREATE TABLE 'nyd_setting' ('id' INTEGER NOT NULL, 'condition_quest_id' INTEGER NOT NULL, 'condition_boss_id' INTEGER NOT NULL, 'complete_emblem_id' INTEGER NOT NULL, PRIMARY KEY('id'));
+
